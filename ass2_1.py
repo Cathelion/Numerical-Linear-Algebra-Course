fbaca0e58be091c5b07081208f512fe979453a0b
Cathelion <61538850+Cathelion@users.noreply.github.com>
2020-09-11 19:58:40 +0200

    Add files via upload
    task 1,2,3,4 included

--- ass2_1.py
+++ ass2_1.py
@@ -25,7 +25,8 @@ class Ortho:
     # projection
     def project(self,u,v):
         return (v@u)/(u@u) * u
-
+    
+    # orthogonalization with GS
     def gramschmidt(self):
         A = self.A
         ortList = []
@@ -38,6 +39,7 @@ class Ortho:
             ortList.append(self.normalize(u_k))
         return np.array(ortList).T
     
+    # checks GS for accuracy
     def test(self):
         Q = self.gramschmidt()
         Z = Q.T @ Q
@@ -46,44 +48,85 @@ class Ortho:
         eig1 = np.max(np.abs(np.linalg.eig(Z)[0]-np.ones(self.n))) 
         det = np.abs(np.abs(np.linalg.det(Z))-1)
         return norm1,norm2,eig1,det
+    
+    # help fcn to compute householder matrix
+    def house(self,A):
+        a=A[:,0]
+        m = A.shape[0]
+        ahat = np.array( [np.linalg.norm(a)] + ( m-1 )*[0.] )
+        v=a- ahat
+        v=v/ np.linalg.norm (v)
+        Q=np.eye( m )- 2* np.outer (v , v )
+        return Q
+    
+    # computes QR factorization
+    def qr(self):
+        A = self.A
+        m = self.m
+        n = self.n
+        pass       
+        A_i = A
+        Q = np.eye(m)
+        for i in range(n):
+            H_ii = self.house(A_i[i:,i:])
+            H_i = np.eye(m)
+            H_i[i:,i:] = H_ii
+            A_i = H_i @ A_i
+            Q = Q @ H_i.T
+        return Q[:,:n], A_i[:n,:]
             
-
-N = [1,10,100,500,1000,2000]
-data =  []
-for n in N:
-    A = np.random.rand(n+2,n)
-    Ort1 = Ortho(A)
-    data.append(Ort1.test())
-    print(n)
-
-for i in range(len(data)):
-    data[i]=list(data[i])
-
-data= np.array(data)
-
-plt.figure(0)
-plt.plot(N, data[:,0],'x', label="2-norm")
-plt.xlabel("Dimension")
-plt.ylabel("Difference of 2-norm to 1")
-plt.legend()
-
-plt.figure(1)
-plt.plot(N, data[:,1],'x', label="Derivation from Identity")
-plt.xlabel("Dimension")
-plt.ylabel("Difference of to Identity")
-plt.legend()
-
-plt.figure(2)
-plt.plot(N, data[:,2],'x', label="Biggest mistake in eigenvalue")
-plt.xlabel("Dimension")
-plt.ylabel("Difference to eigenvalue 1")
-plt.legend()
         
-plt.figure(3)
-plt.plot(N, data[:,3],'x', label="Determinant")
-plt.xlabel("Dimension")
-plt.ylabel("Difference to determinant")
-plt.legend()
+# test own QR against built-in
+def QR_test():
+    N = [1,10,100,500,1000,2000]
+    for n in N:
+        A = np.random.rand(n+2,n)
+        Ort1 = Ortho(A)
+        Q1, R1 = Ort1.qr()
+        Q2, R2 = np.linalg.qr(A)
+        Q_err = np.linalg.norm(np.abs(Q1)-np.abs(Q2),2)
+        R_err = np.linalg.norm(np.abs(R1)-np.abs(R2),2)
+        print("Dimension:",n,"  -  Q error: ", Q_err, "   R error: ", R_err)
+ 
+# testing and plotting of the errors of GS            
+def test_and_plot():
+    N = [1,10,100,500,1000,2000]
+    data =  []
+    for n in N:
+        A = np.random.rand(n+2,n)
+        Ort1 = Ortho(A)
+        data.append(Ort1.test())
+        print(n)
+
+
+    for i in range(len(data)):
+        data[i]=list(data[i])
+    
+    data= np.array(data)
+    
+    plt.figure(0)
+    plt.plot(N, data[:,0],'x', label="2-norm")
+    plt.xlabel("Dimension")
+    plt.ylabel("Difference of 2-norm to 1")
+    plt.legend()
+    
+    plt.figure(1)
+    plt.plot(N, data[:,1],'x', label="Derivation from Identity")
+    plt.xlabel("Dimension")
+    plt.ylabel("Difference of to Identity")
+    plt.legend()
+    
+    plt.figure(2)
+    plt.plot(N, data[:,2],'x', label="Biggest mistake in eigenvalue")
+    plt.xlabel("Dimension")
+    plt.ylabel("Difference to eigenvalue 1")
+    plt.legend()
+            
+    plt.figure(3)
+    plt.plot(N, data[:,3],'x', label="Determinant")
+    plt.xlabel("Dimension")
+    plt.ylabel("Difference to determinant")
+    plt.legend()
         
         
         
